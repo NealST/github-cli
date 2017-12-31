86c3b0fde081c4db4ef7aff1972b50c80e4edd20
mozheng <mozheng@mogujie.com>
2017-12-31 13:12:18 +0800

    github cli 代码提交

---
+++ src/types/chalk.d.ts
@@ -0,0 +1,8 @@
+declare module 'chalk' {
+  export function magentaBright (message: string): string
+  export function yellow (message: string): string
+  export function gray (message: string): string
+  export function green (message: string): string
+  export function cyan (message: string): string
+  export function red (message: string): string
+}
